6ccc4000405b57b633d53df70b5991b02a18be41
ksmin23 <sonagy23@gmail.com>
2020-10-10 08:54:20 +0000

    creates Neptune Workbench

--- neptune_tutorial/neptune_tutorial_stack.py
+++ neptune_tutorial/neptune_tutorial_stack.py
@@ -7,7 +7,9 @@ from aws_cdk import (
   core,
   aws_ec2,
   aws_s3 as s3,
-  aws_neptune
+  aws_neptune,
+  aws_iam,
+  aws_sagemaker
 )
 
 
@@ -101,3 +103,65 @@ class NeptuneTutorialStack(core.Stack):
     )
     graph_db_replica_instance.add_depends_on(graph_db)
     graph_db_replica_instance.add_depends_on(graph_db_instance)
+
+    sagemaker_notebook_role_policy_doc = aws_iam.PolicyDocument()
+    sagemaker_notebook_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "effect": aws_iam.Effect.ALLOW,
+      "resources": ["arn:aws:s3:::aws-neptune-notebook",
+        "arn:aws:s3:::aws-neptune-notebook/*"],
+      "actions": ["s3:GetObject",
+        "s3:ListBucket"]
+    }))
+
+    sagemaker_notebook_role_policy_doc.add_statements(aws_iam.PolicyStatement(**{
+      "effect": aws_iam.Effect.ALLOW,
+      "resources": ["arn:aws:neptune-db:{region}:{account}:cluster-*/*".format(
+        region=core.Aws.REGION, account=core.Aws.ACCOUNT_ID)],
+      "actions": ["neptune-db:connect"]
+    }))
+
+    sagemaker_notebook_role = aws_iam.Role(self, 'SageMakerNotebookForNeptuneWorkbenchRole',
+      role_name='AWSNeptuneNotebookRole-NeptuneTutorial',
+      assumed_by=aws_iam.ServicePrincipal('sagemaker.amazonaws.com'),
+      #XXX: use inline_policies to work around https://github.com/aws/aws-cdk/issues/5221
+      inline_policies={
+        'AWSNeptuneNotebook': sagemaker_notebook_role_policy_doc
+      }
+    )
+
+    neptune_wb_lifecycle_content = '''#!/bin/bash
+sudo -u ec2-user -i <<'EOF'
+
+echo "export GRAPH_NOTEBOOK_AUTH_MODE=DEFAULT" >> ~/.bashrc
+echo "export GRAPH_NOTEBOOK_HOST={NeptuneClusterEndpoint}" >> ~/.bashrc
+echo "export GRAPH_NOTEBOOK_PORT={NeptuneClusterPort}" >> ~/.bashrc
+echo "export NEPTUNE_LOAD_FROM_S3_ROLE_ARN=''" >> ~/.bashrc
+echo "export AWS_REGION={AWS_Region}" >> ~/.bashrc
+
+aws s3 cp s3://aws-neptune-notebook/graph_notebook.tar.gz /tmp/graph_notebook.tar.gz
+rm -rf /tmp/graph_notebook
+tar -zxvf /tmp/graph_notebook.tar.gz -C /tmp
+/tmp/graph_notebook/install.sh
+EOF
+'''.format(NeptuneClusterEndpoint=graph_db.attr_endpoint,
+    NeptuneClusterPort=graph_db.attr_port,
+    AWS_Region=core.Aws.REGION)
+
+    neptune_wb_lifecycle_config_prop = aws_sagemaker.CfnNotebookInstanceLifecycleConfig.NotebookInstanceLifecycleHookProperty(
+      content=core.Fn.base64(neptune_wb_lifecycle_content)
+    )
+
+    neptune_wb_lifecycle_config = aws_sagemaker.CfnNotebookInstanceLifecycleConfig(self, 'NpetuneWorkbenchLifeCycleConfig',
+      notebook_instance_lifecycle_config_name='NeptuneWorkbenchLifeCycleConfig',
+      on_start=[neptune_wb_lifecycle_config_prop]
+    )
+
+    neptune_workbench = aws_sagemaker.CfnNotebookInstance(self, 'NeptuneWorkbench',
+      instance_type='ml.t2.medium',
+      role_arn=sagemaker_notebook_role.role_arn,
+      lifecycle_config_name=neptune_wb_lifecycle_config.notebook_instance_lifecycle_config_name,
+      notebook_instance_name='NeptuneTutorialWorkbench',
+      root_access='Disabled',
+      security_group_ids=[sg_use_graph_db.security_group_name],
+      subnet_id=graph_db_subnet_group.subnet_ids[0]
+    )
